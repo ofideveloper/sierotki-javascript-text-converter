47d602d70a76212f1760d9956337df368d89e838
dominikofi <dominik.ofiara@brasilwarsaw.pl>
2016-12-03 20:08:19 +0100

    Added simple notify msg

--- src/app/main.js
+++ src/app/main.js
@@ -6,10 +6,11 @@ import Clipboard from 'clipboard'
 $(document).ready(function() {
 	
 	let App = function() {
-		let init, setupEvents, convertSpaces,
+		let init, setupEvents, convertSpaces, notify,
 			$btnConvert = $('.btn-convert'),
 			$textArea = $('.textarea'),
 			$textArea2 = $('.textarea2'),
+			$alert = $('.alert'),
 			// Sierotki array copied from Wordpress Plugin -> check this here https://pl.wordpress.org/plugins/sierotki/
 			sierotki = ['al.', 'ale', 'ależ', 'b.', 'bm.', 'bp', 'br.', 'by', 'bym', 'byś', 'bł.', 'cyt.', 'cz.', 'czyt.', 'dn.', 'do', 'doc.', 'dr', 'ds.', 'dyr.', 'dz.', 'fot.', 'gdy', 'gdyby', 'gdybym', 'gdybyś', 'gdyż', 'godz.', 'im.', 'inż.', 'jw.', 'kol.', 'komu', 'ks.', 'która', 'którego', 'której', 'któremu', 'który', 'których', 'którym', 'którzy', 'lic.', 'm.in.', 'max', 'mgr', 'min', 'moich', 'moje', 'mojego', 'mojej', 'mojemu', 'mych', 'mój', 'na', 'nad', 'np.', 'nr', 'nr.', 'nrach', 'nrami', 'nrem', 'nrom', 'nrowi', 'nru', 'nry', 'nrze', 'nrze', 'nrów', 'nt.', 'nw.', 'od', 'oraz', 'os.', 'p.', 'pl.', 'pn.', 'po', 'pod', 'pot.', 'prof.', 'przed', 'pt.', 'pw.', 'pw.', 'tamtej', 'tamto', 'tej', 'tel.', 'tj.', 'to', 'twoich', 'twoje', 'twojego', 'twojej', 'twych', 'twój', 'ul.', 'we', 'wg', 'woj.', 'za', 'ze', 'śp.', 'św.', 'że', 'żeby', 'żebyś' ]
 
@@ -18,7 +19,13 @@ $(document).ready(function() {
 
 			clipboard = new Clipboard('.btn-copy');
 
+			clipboard.on('success', function(e) {
+				notify('success')
+			});
 
+			clipboard.on('error', function(e) {
+				notify('error') 
+			});
 
 			$('textarea').on('focus', function(event) {
 				event.preventDefault()
@@ -37,8 +44,32 @@ $(document).ready(function() {
 					convertSpaces($textArea.val())
 				}, 100)
 			})
+		}
 
+		notify = function(msg) {
+			let $alertMessage = $alert.find('p')
 
+			$alert.removeClass('success, error')
+			$alertMessage.html('')
+
+			switch (msg) {
+				case 'error':
+					$alert.addClass('error')
+					$alertMessage.html('Wystąpił błąd')
+
+					setTimeout(function() {
+						$('.alert').removeClass('error')
+					}, 600)
+
+					break
+				default:
+					$alert.addClass('success')
+					$alertMessage.html('Skopiowano do schowka')
+
+					setTimeout(function() {
+						$('.alert').removeClass('success')
+					}, 600)
+			}
 		}
 
 		convertSpaces = function(text) {
@@ -77,6 +108,8 @@ $(document).ready(function() {
 
 			return
 		}
+
+
 
 		init = function() {
 			setupEvents()
